702ef1fe5b7713a38600d03353b5b8ee485daa7f
Fabiano Raiser <113053557+FabianoRaiser@users.noreply.github.com>
2025-04-22 20:54:17 -0300

    Correção na operação

--- math.c
+++ math.c
@@ -11,7 +11,7 @@ int sub(int a, int b) {
 int main() {
     soma(1, 2);
     sub(5, 1);
-    double op = soma / sub;
-    printf("O resultado é: %f", op);
+    double op = soma * sub;
+    printf("O resultado é: %f\n", op);
     return 0;
 }
